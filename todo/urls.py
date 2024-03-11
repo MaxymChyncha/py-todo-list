6a85d6cf61b08cc6cdf9a5f03cde24ccb7824a05
MaxymChyncha <max.chyncha@gmail.com>
2024-03-11 11:28:40 +0200

    feat(todo): add index view and url

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from todo.views import IndexView
+
 app_name = "todo"
 
-urlpatterns = []
+urlpatterns = [
+    path("", IndexView.as_view(), name="index")
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class IndexView(generic.ListView):
+    model = Task
+    paginate_by = 5
+    template_name = "todo/index.html"
